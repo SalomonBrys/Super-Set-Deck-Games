d2368abdcbc8d4aff542afa9d4d0cd5a67e03fc4
Salomon BRYS <salomon.brys@gmail.com>
2023-01-29 20:31:23 +0100

    Better Share

--- App/src/jsMain/kotlin/Game.kt
+++ App/src/jsMain/kotlin/Game.kt
@@ -228,6 +228,7 @@ fun RouteBuilder.Game(game: Game?, langMenu: LangMenu) {
                                             "link" -> {
                                                 val share = json(
                                                     "title" to gameName,
+                                                    "text" to gameName,
                                                     "url" to "https://super-set-deck.games/#/game/${game?.id}"
                                                 )
                                                 window.navigator.asDynamic().share(share)
